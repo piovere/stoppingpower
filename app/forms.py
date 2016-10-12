fc6d4baf58b106aedab8f2ddcfba0195b13a0422
J.R. Powers-Luhn <jpowersl@vols.utk.edu>
2016-10-12 14:08:22 -0400

    IT WORKS!

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,4 @@
-from flask.ext.wtf import Form
+from flask_wtf import Form
 from wtforms import BooleanField, IntegerField, DecimalField, SelectField
 from wtforms.validators import DataRequired
 from stoppingpower.materials import materials
@@ -8,6 +8,32 @@ class StoppingPowerForm(Form):
     incident_z = IntegerField('incident_z', validators=[DataRequired()])
     incident_a = IntegerField('incident_a', validators=[DataRequired()])
     incident_t = DecimalField('incident_t', validators=[DataRequired()])
-    per_nucleon_bool = BooleanField('per_nucleon_bool', validators=[DataRequired()])
+    per_nucleon_bool = BooleanField('per_nucleon_bool')
     material = SelectField('material',
                            choices=zip(materials.keys(), materials.keys()))
+
+
+class RangeForm(Form):
+    incident_z = IntegerField('incident_z', validators=[DataRequired()])
+    incident_a = IntegerField('incident_a', validators=[DataRequired()])
+    incident_t = DecimalField('incident_t', validators=[DataRequired()])
+    per_nucleon_bool = BooleanField('per_nucleon_bool')
+    material = SelectField('material',
+                           choices=zip(materials.keys(), materials.keys()))
+
+
+class ExitForm(Form):
+    incident_z = IntegerField('incident_z', validators=[DataRequired()])
+    incident_a = IntegerField('incident_a', validators=[DataRequired()])
+    incident_t = DecimalField('incident_t', validators=[DataRequired()])
+    per_nucleon_bool = BooleanField('per_nucleon_bool')
+    material = SelectField('material',
+                           choices=zip(materials.keys(), materials.keys()))
+    thickness = DecimalField('thickness', validators=[DataRequired()])
+
+
+class NuclearInteractionFractionForm(Form):
+    incident_a = IntegerField('incident_a', validators=[DataRequired()])
+    material = SelectField('material',
+                           choices=zip(materials.keys(), materials.keys()))
+    thickness = DecimalField('thickness', validators=[DataRequired()])
--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
-from flask import render_template, flash, redirect
+from flask import render_template
 from app import app
-from stoppingpower.cmsp import *
+from stoppingpower.cmsp import S_c
+from stoppingpower.range import rangeout, exit_energy
 from stoppingpower.materials import materials
-from forms import StoppingPowerForm
+from stoppingpower.range import nuclear_fraction
+from forms import StoppingPowerForm, RangeForm, ExitForm, NuclearInteractionFractionForm
 
 @app.route('/')
 @app.route('/index')
@@ -15,20 +17,83 @@ def index():
 @app.route('/cmsp', methods=['GET', 'POST'])
 def cmsp_page():
     form = StoppingPowerForm()
-    print form.errors
+
     if form.validate_on_submit():
-        if form.per_nucleon_bool is True:
+        if form.per_nucleon_bool.data is True:
             t = float(form.incident_t.data) * float(form.incident_a.data)
         else:
             t = float(form.incident_t.data)
-        poo = S_c(
-            float(form.incident_z.data),
-            materials[form.material.data],
-            t,
-            float(form.incident_a.data)
-        )
+        sc = S_c(float(form.incident_z.data), materials[form.material.data], t, float(form.incident_a.data))
     else:
-        poo = None
+        sc = None
     return render_template('cmsp.html',
                            form=form,
-                           poo=poo)
+                           sc=sc)
+
+
+@app.route('/range', methods=['GET', 'POST'])
+def range_page():
+    form = RangeForm()
+
+    if form.validate_on_submit():
+        if form.per_nucleon_bool.data is True:
+            t = float(form.incident_t.data) * float(form.incident_a.data)
+        else:
+            t = float(form.incident_t.data)
+        z = float(form.incident_z.data)
+        a = float(form.incident_a.data)
+        r = rangeout(z, materials[form.material.data], t, a)
+    else:
+        r = None
+    return render_template(
+        'range.html',
+        form=form,
+        r=r
+    )
+
+
+@app.route('/exit', methods=['GET', 'POST'])
+def exit_energy_page():
+    form = ExitForm()
+
+    if form.validate_on_submit():
+        if form.per_nucleon_bool.data is True:
+            t = float(form.incident_t.data) * float(form.incident_a.data)
+        else:
+            t = float(form.incident_t.data)
+        z = float(form.incident_z.data)
+        a = float(form.incident_a.data)
+        thickness = float(form.thickness.data)
+        ee = exit_energy(z, materials[form.material.data], t, a, thickness)
+        eepn = ee / a
+        deposited = t - ee
+    else:
+        ee = None
+        eepn = None
+        deposited = None
+    return render_template(
+        'exitenergy.html',
+        form=form,
+        ee=ee,
+        eepn=eepn,
+        deposited=deposited
+    )
+
+
+@app.route('/nuclear', methods=['GET', 'POST'])
+def nuclear_interaction_fraction_page():
+    form = NuclearInteractionFractionForm()
+
+    if form.validate_on_submit():
+        inter_frac = nuclear_fraction(
+            float(form.incident_a.data),
+            materials[form.material.data],
+            float(form.thickness.data)
+        )
+    else:
+        inter_frac = None
+    return render_template(
+        'nuclear.html',
+        form=form,
+        inter_frac=inter_frac
+    )
