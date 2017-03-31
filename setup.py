aa997bd556acc6f2125850538dfb7835acbfc678
Justin Powers-Luhn <jrpl@gardiner.dreamhost.com>
2017-03-31 14:25:59 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='stoppingpower',
-    version='0.1',
+    version='0.1.1',
     packages=['stoppingpower', 'tests'],
     url='',
     license='MIT',
